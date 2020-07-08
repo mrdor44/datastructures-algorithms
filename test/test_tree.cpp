74bcb79a574d8d119e550b8b6562e6ca3a248363
Dor <mrdor44@gmail.com>
2020-07-08 22:19:02 +0300

    Extracted fixture

--- test/test_tree.cpp
+++ test/test_tree.cpp
@@ -7,24 +7,25 @@ class TreeTest : public Test {
 protected:
     void SetUp() override {
         Test::SetUp();
+
+        //     10
+        //    /  \
+        //   5    7
+        //  / \    \
+        // 9   2    1
+        tree = Tree<int>::create(10);
+        const Tree<int>::NodePtr& c5 = tree->set_left(5);
+        const Tree<int>::NodePtr& c7 = tree->set_right(7);
+        c5->set_left(9);
+        c5->set_right(2);
+        c7->set_right(1);
     }
+
+    Tree<int>::NodePtr tree;
 };
 
-TEST(Tree, PreOrder) {
-    //     10
-    //    /  \
-    //   5    7
-    //  / \    \
-    // 9   2    1
-    const Tree<int>::NodePtr& r10 = Tree<int>::create(10);
-    const Tree<int>::NodePtr& c5 = r10->set_left(5);
-    const Tree<int>::NodePtr& c7 = r10->set_right(7);
-    c5->set_left(9);
-    c5->set_right(2);
-    c7->set_right(1);
-
+TEST_F(TreeTest, PreOrder) {
     List<int> items;
-    Tree<int>::do_preorder(r10, [&](const int& item) { items.push_back(item); });
-
+    Tree<int>::do_preorder(tree, [&](const int& item) { items.push_back(item); });
     EXPECT_EQ(items, List<int>({10, 5, 9, 2, 7, 1}));
 }
