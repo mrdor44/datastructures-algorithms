def4d8dc6d98246f9c36d890f00566307d7b5325
Dor <mrdor44@gmail.com>
2020-07-25 23:31:59 +0300

    Change to in-place partition, doesn't work

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,6 +1,17 @@
 from statistics import median
 
 
+def _partition(lst, pivot_index):
+    next_below_pivot_index = 0
+    for i, value in enumerate(lst):
+        if value > lst[pivot_index] or i == pivot_index:
+            continue
+        lst[i], lst[next_below_pivot_index] = lst[next_below_pivot_index], lst[i]
+        next_below_pivot_index += 1
+    lst[next_below_pivot_index], lst[pivot_index] = lst[pivot_index], lst[next_below_pivot_index]
+    return next_below_pivot_index
+
+
 def _get_pivot_index(lst):
     candidate_indices = [0, len(lst) // 2, -1]
     m = median(map(lst.__getitem__, candidate_indices))
@@ -12,8 +23,8 @@ def quicksort(lst):
         return []
 
     pivot_index = _get_pivot_index(lst)
-    left = [e for e in lst if e <= lst[pivot_index]]
-    left.remove(lst[pivot_index])
-    right = [e for e in lst if e > lst[pivot_index]]
+    pivot_index = _partition(lst, pivot_index)
+    left = lst[:pivot_index]
+    right = lst[pivot_index + 1:]
 
     return quicksort(left) + [lst[pivot_index]] + quicksort(right)
