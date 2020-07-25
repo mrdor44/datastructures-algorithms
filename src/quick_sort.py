276ccff4583cf6dbd628d495d8848b2066b2020a
Dor <mrdor44@gmail.com>
2020-07-25 23:21:52 +0300

    Changed pivot to index

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,17 +1,19 @@
 from statistics import median
 
 
-def _get_pivot(lst):
-    return median([lst[0], lst[len(lst) // 2], lst[-1]])
+def _get_pivot_index(lst):
+    candidate_indices = [0, len(lst) // 2, -1]
+    m = median(map(lst.__getitem__, candidate_indices))
+    return next(i for i in candidate_indices if lst[i] == m)
 
 
 def quicksort(lst):
     if len(lst) == 0:
         return []
 
-    pivot = _get_pivot(lst)
-    left = [e for e in lst if e <= pivot]
-    left.remove(pivot)
-    right = [e for e in lst if e > pivot]
+    pivot_index = _get_pivot_index(lst)
+    left = [e for e in lst if e <= lst[pivot_index]]
+    left.remove(lst[pivot_index])
+    right = [e for e in lst if e > lst[pivot_index]]
 
-    return quicksort(left) + [pivot] + quicksort(right)
+    return quicksort(left) + [lst[pivot_index]] + quicksort(right)
