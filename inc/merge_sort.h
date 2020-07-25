c8b78e078cbf4e5039296b561d754dbb24bacdb5
Dor <mrdor44@gmail.com>
2020-07-25 18:34:30 +0300

    Changed to C, theres a bug

---
+++ inc/merge_sort.h
@@ -0,0 +1,21 @@
+//
+// Created by dor on 24/07/2020.
+//
+
+#ifndef DATASTRUCTURES_ALGORITHMS_MERGE_SORT_H
+#define DATASTRUCTURES_ALGORITHMS_MERGE_SORT_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#define FREE(p)         \
+do {                    \
+    if (NULL != (p)) {  \
+        free((p));      \
+        (p) = NULL;     \
+    }                   \
+} while (false)
+
+int* merge_sort(int array[], int length);
+
+#endif //DATASTRUCTURES_ALGORITHMS_MERGE_SORT_H
