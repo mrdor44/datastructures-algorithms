782b99b86d6f462936e3642687362cde376122c8
Dor <mrdor44@gmail.com>
2020-07-27 09:26:12 +0300

    Refactored stringbuilder

--- inc/common.h
+++ inc/common.h
@@ -2,6 +2,7 @@
 #define DATASTRUCTURES_ALGORITHMS_COMMON_H
 
 #include <stdlib.h>
+#include "inc/common.h"
 
 #define CLOSE(p, pfn)       \
 do {                        \
@@ -12,5 +13,17 @@ do {                        \
 } while (false)
 
 #define FREE(p) CLOSE((p), free)
+
+typedef enum {
+    RETURNCODE_INVALID_VALUE = -1,
+    RETURNCODE_SUCCESS = 0,
+
+    RETURNCODE_STRINGBUILDER_APPEND_INVALID_PARAMETERS,
+    RETURNCODE_STRINGBUILDER_APPEND_NODE_CREATE_FAILED,
+    RETURNCODE_STRINGBUILDER_STR_INVALID_PARAMETERS,
+    RETURNCODE_STRINGBUILDER_STR_MALLOC_FAILED,
+
+    RETURNCODE_MINHEAP_GET_MIN_INVALID_PARAMETERS
+} t_returncode;
 
 #endif //DATASTRUCTURES_ALGORITHMS_COMMON_H
--- test/test_string_builder.cpp
+++ test/test_string_builder.cpp
@@ -1,17 +1,16 @@
 #include "inc/string_builder.hpp"
 #include "mock.hpp"
+#include "test/common.hpp"
 
 using testing::NotNull;
-
-#define EXPECT_STRINGBUILDER_SUCEESS(expr) EXPECT_EQ(STRINGBUILDER_RETURNCODE_SUCCESS, (expr))
 
 TEST(StringBuilder, Build) {
     t_stringbuilder sb = STRINGBUILDER_create();
     EXPECT_THAT(sb, NotNull());
-    EXPECT_STRINGBUILDER_SUCEESS(STRINGBUILDER_append(sb, "GREAT "));
-    EXPECT_STRINGBUILDER_SUCEESS(STRINGBUILDER_append(sb, "SUCCESS!"));
+    EXPECT_SUCCESS(STRINGBUILDER_append(sb, "GREAT "));
+    EXPECT_SUCCESS(STRINGBUILDER_append(sb, "SUCCESS!"));
     char* str = nullptr;
-    EXPECT_STRINGBUILDER_SUCEESS(STRINGBUILDER_str(sb, &str));
+    EXPECT_SUCCESS(STRINGBUILDER_str(sb, &str));
     EXPECT_STREQ("GREAT SUCCESS!", str);
     FREE(str);
     STRINGBUILDER_DESTROY(sb);
